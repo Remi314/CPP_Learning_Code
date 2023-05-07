44fe0f8076d183a9f041aa21867d4a6a349b9804
Remi314 <matcheremi9@gmail.com>
2023-05-07 18:34:04 +0200

    Chapter 1 done!

--- chap-01/4-calculator.cpp
+++ chap-01/4-calculator.cpp
@@ -1,66 +1,119 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
-int main(int argc, char** argv)
+// The result
+void display_result(int result)
 {
-    // Parsing program parameters.
+    std::cout << "Result is " << result << std::endl;
+}
+
+// Check the operation
+int add(const std::vector<int> &values)
+{
+    int result = 0;
+    for (auto v : values)
+    {
+        result += v;
+    }
+    return result;
+}
+
+int multiply(const std::vector<int> &values)
+{
+    auto result = 1;
+    for (auto v : values)
+    {
+        result *= v;
+    }
+    return result;
+}
+
+int sub(const std::vector<int> &values)
+{
+    auto result = values[0];
+    for (auto i = 1; i < values.size(); ++i)
+    {
+        result -= values[i];
+    }
+    return result;
+}
+
+int compute_result(char op, const std::vector<int> &values)
+{
+    switch (op)
+    {
+    case '+':
+        return add(values);
+
+    case '*':
+        return multiply(values);
+
+    case '-':
+        return sub(values);
+
+    default:
+        return 0;
+    }
+}
+
+/*
+Cette fonction verifie trois informations :
+- si les arguments sont valides ou non,
+- l’opérateur,
+- les opérandes.
+*/
+bool parse_params(char& op, std::vector<int>& values, int argc, char **argv)
+{
     if (argc < 2)
     {
         std::cerr << "Expected operator as first argument." << std::endl;
-        return -1;
+        return false;
     }
 
-    std::string op = argv[1];
-
-    if (op != "+" && op != "*" && op != "-")
+    std::string op_str = argv[1];
+    if (op_str != "+" && op_str != "*" && op_str != "-")
     {
         std::cerr << "Expected operator to be '+', '*' or '-'." << std::endl;
-        return -1;
+        return false;
     }
 
-    std::vector<int> values;
+
     for (auto arg_i = 2; arg_i < argc; ++arg_i)
     {
         auto value = std::stoi(argv[arg_i]);
         values.emplace_back(value);
     }
 
+    op = op_str[0];
+    if (op == '-' && values.empty())
+    {
+        std::cerr << "Operator '-' expects at least one operand to substract from." << std::endl;
+        return -1;
+    }
+
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    // Parsing program parameters.
+    char op = '?';
+    std::vector<int> values;
+
+    if (!parse_params(op, values, argc, argv))
+    {
+        return -1;
+    }
+
     // Process operation, depending on the operator.
-    auto result = 0;
+    auto result = compute_result(op, values);
 
-    if (op == "+")
-    {
-        for (auto v : values)
-        {
-            result += v;
-        }
-    }
-    else if (op == "*")
-    {
-        result = 1;
-        for (auto v : values)
-        {
-            result *= v;
-        }
-    }
-    else if (op == "-")
-    {
-        if (values.empty())
-        {
-            std::cerr << "Operator '-' expects at least one operand to substract from." << std::endl;
-            return -1;
-        }
-
-        result = values[0];
-        for (auto i = 1u; i < values.size(); ++i)
-        {
-            result -= values[i];
-        }
-    }
-
-    // Output result.
-    std::cout << "Result is " << result << std::endl;
+    // Return result
+    display_result(result);
 
     return 0;
 }
+
+// For exemple: .\hello.exe + 4 5
